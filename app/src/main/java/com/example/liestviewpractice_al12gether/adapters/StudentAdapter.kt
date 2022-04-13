69023b8f71261083c013ca58727cd8c968e1d2aa
al12gether <alonetogetherpd@gmail.com>
2022-04-13 17:46:52 +0900

    Adapter 뼈대 작업 마무리

--- app/src/main/java/com/example/liestviewpractice_al12gether/adapters/StudentAdapter.kt
+++ app/src/main/java/com/example/liestviewpractice_al12gether/adapters/StudentAdapter.kt
@@ -1,7 +1,11 @@
 package com.example.liestviewpractice_al12gether.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.example.liestviewpractice_al12gether.R
 import com.example.liestviewpractice_al12gether.datas.Student
 import java.util.ArrayList
 import kotlin.collections.ArrayList
@@ -10,4 +14,21 @@ class StudentAdapter(
     val mContext : Context,
     val resId : Int,
     val mList : ArrayList<Student>) : ArrayAdapter<Student>(mContext, resId, mList) {
+
+    val inf = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+        if (tempRow == null) {
+            tempRow = inf.inflate(R.layout.student_list_item, null)
+        }
+
+        val row = tempRow!!
+
+
+        return row
+
+    }
+
 }
